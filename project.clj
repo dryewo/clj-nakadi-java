f55f4b7ea5bcb83acbf27839a60308d11a88f92f
Dmitrii Balakhonskii <dnomaid@gmail.com>
2019-01-23 21:18:24 +0100

    use Clojure 1.10 for dev

--- project.clj
+++ project.clj
@@ -21,4 +21,4 @@
                   ["deploy"]
                   ["vcs" "push"]]
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}})
